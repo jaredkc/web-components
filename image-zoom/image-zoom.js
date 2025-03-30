1589ce4463d1c20652472fdfec5b2a5761466277
jaredkc <jaredkc@gmail.com>
2025-03-30 10:31:16 -0600

    Working basic image zoom component

--- image-zoom/image-zoom.js
+++ image-zoom/image-zoom.js
@@ -3,20 +3,37 @@ if (!customElements.get('image-zoom')) {
     constructor() {
       super();
       this.image = this.querySelector('img');
+      this.scale = 4;
     }
 
     connectedCallback() {
-      this.image.addEventListener('click', this.handleClick.bind(this));
+      this.addEventListener('click', this.handleClick.bind(this));
     }
 
     handleClick(event) {
-      console.log('ImageZoom clicked');
-      if (this.image.style.width === '300%') {
-        this.image.removeAttribute('style');
+      const rect = this.getBoundingClientRect();
+
+      // Calculate relative click position as percentage
+      const relativeX = (event.clientX - rect.left) / rect.width;
+      const relativeY = (event.clientY - rect.top) / rect.height;
+
+      if (this.image.style.width) {
+        this.style.height = '';
+        this.image.style.width = '';
+        this.image.style.maxWidth = '';
       } else {
-        this.image.style.width = '300%';
-        this.image.style.maxWidth = '300%';
-        this.image.style.height = 'auto';
+        this.style.height = rect.height + 'px';
+        this.image.style.width = `${this.scale * 100}%`;
+        this.image.style.maxWidth = `${this.scale * 100}%`;
+
+        // Calculate the new dimensions
+        const newWidth = rect.width * this.scale;
+        const newHeight = rect.height * this.scale;
+
+        this.scrollTo({
+          left: newWidth * relativeX - rect.width / 2,
+          top: newHeight * relativeY - rect.height / 2,
+        });
       }
     }
   }
